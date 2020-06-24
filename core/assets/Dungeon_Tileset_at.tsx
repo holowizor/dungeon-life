ca21fc5e3ee419459e3f6dd1dd604df799e05e0e
darkone6666 <oliwier.sosna.kwiecien@gmail.com>
2020-06-24 09:49:54 +0200

    dungeon level one

---
+++ core/assets/Dungeon_Tileset_at.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.0" name="Dungeon_Tileset_at" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+ <image source="Dungeon_Tileset_at.png" trans="ff00ff" width="512" height="512"/>
+</tileset>
